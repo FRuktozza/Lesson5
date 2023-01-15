5b5fcd770f493d9b8e7469a9c07fbb059fb34782
FRuktozza <121963443+FRuktozza@users.noreply.github.com>
2023-01-15 11:56:56 +0300

    Add files via upload

---
+++ Task37/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите размер массива");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[n];
+
+        FillArray(ref array);
+        int[] pairs = MultiplyPairs(array);
+        Console.WriteLine(NicePrint(pairs));
+    }
+
+    static void FillArray(ref int[] array)
+    {
+        Console.WriteLine("Введите элементы массива");
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    static int[] MultiplyPairs(int[] array)
+    {
+        int[] pairs = new int[array.Length / 2];
+        for (int i = 0; i < pairs.Length; i++)
+        {
+            pairs[i] = array[i] * array[array.Length - 1 - i];
+        }
+
+        return pairs;
+    }
+
+    static string NicePrint(int[] array)
+    {
+        return $"Умноженные пары: {String.Join(", ", array)}";
+    }
+}
